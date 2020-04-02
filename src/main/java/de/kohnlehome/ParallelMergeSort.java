6979aea42f345aa5c6986963bd08c2e0e9b1a2cf
LuiVanCorny <ludwig.korn.96@googlemail.com>
2020-04-02 11:48:30 +0200

    deleted forkjoinpool

--- src/main/java/de/kohnlehome/ParallelMergeSort.java
+++ src/main/java/de/kohnlehome/ParallelMergeSort.java
@@ -4,17 +4,15 @@ import java.util.concurrent.ForkJoinPool;
 
 public class ParallelMergeSort implements IMergeSort{
     private IMerger merger;
-    private ForkJoinPool forkJoinPool;
 
     public ParallelMergeSort(IMerger merger) {
         this.merger = merger;
-        this.forkJoinPool = new ForkJoinPool();
     }
 
     @Override
     public int[] sort(int[] unsortedArray) {
         SortierenTask sortierenTask = new SortierenTask(unsortedArray, merger);
-        forkJoinPool.invoke(sortierenTask);
+        sortierenTask.invoke();
 
         return sortierenTask.join();
     }
