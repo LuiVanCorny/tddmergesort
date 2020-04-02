439fa95bfcc9b96b969975fca69a5466b7ca6c3e
LuiVanCorny <ludwig.korn.96@googlemail.com>
2020-04-02 12:51:45 +0200

    implementing ArraysParallelSort with Testclass

---
+++ src/main/java/de/kohnlehome/ArraysParallelSort.java
@@ -0,0 +1,13 @@
+package de.kohnlehome;
+
+import java.util.Arrays;
+
+public class ArraysParallelSort implements ISortAlgorithm {
+    @Override
+    public int[] sort(int[] unsortedArray) {
+
+        Arrays.parallelSort(unsortedArray);
+
+        return unsortedArray;
+    }
+}
