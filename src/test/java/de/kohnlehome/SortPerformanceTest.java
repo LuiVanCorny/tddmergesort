9bc3c862792c582d845bb66146b754feb43ffc54
LuiVanCorny <ludwig.korn.96@googlemail.com>
2020-04-02 12:44:26 +0200

    refactoring the performance test

--- src/test/java/de/kohnlehome/SortPerformanceTest.java
+++ src/test/java/de/kohnlehome/SortPerformanceTest.java
@@ -7,28 +7,30 @@ import java.util.Random;
 public class SortPerformanceTest {
 
     private final int size = 100000000;
-
+    private int[] unsortedArray;
 
     @Test
     public void testPerformance(){
 
         //großes Array mit Zufallszahlen erstellen
         Random rand = new Random();
-        int[] unsortedArray = new int[size];
+       unsortedArray = new int[size];
         for(int i = 0; i< size;i++){
             unsortedArray[i] = rand.nextInt(size+1);
         }
 
-        doAnPerformanceTest(unsortedArray, new ParallelMergeSort(new Merger()));
-        doAnPerformanceTest(unsortedArray, new MergeSort(new Merger()));
+        doAnPerformanceTest(new ParallelMergeSort(new Merger()));
+        doAnPerformanceTest(new MergeSort(new Merger()));
 
     }
 
-    private void doAnPerformanceTest(int[] unsortedArray, ISortAlgorithm sortAlgorithm){
+    private void doAnPerformanceTest(ISortAlgorithm sortAlgorithm){
+
+        int[] copyArray = unsortedArray.clone();
         //Startzeit messen
         long start = System.nanoTime();
         //sortieren
-        sortAlgorithm.sort(unsortedArray);
+        sortAlgorithm.sort(copyArray);
         //Endzeit messen
         long ende = System.nanoTime();
         //Differenz berechnen
