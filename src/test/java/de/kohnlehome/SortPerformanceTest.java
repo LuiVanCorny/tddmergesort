675879c558a9bb6e062c73c2016da2a4d7ce0638
LuiVanCorny <ludwig.korn.96@googlemail.com>
2020-04-02 12:09:32 +0200

    refactoring the performance test

--- src/test/java/de/kohnlehome/SortPerformanceTest.java
+++ src/test/java/de/kohnlehome/SortPerformanceTest.java
@@ -8,38 +8,32 @@ public class SortPerformanceTest {
 
     private final int size = 100000000;
 
+
     @Test
     public void testPerformance(){
+
         //großes Array mit Zufallszahlen erstellen
         Random rand = new Random();
         int[] unsortedArray = new int[size];
-
         for(int i = 0; i< size;i++){
             unsortedArray[i] = rand.nextInt(size+1);
         }
 
-        //Startzeit messen ParallelSort
-        long parallelStart = System.nanoTime();
+        doAnPerformanceTest(unsortedArray, new ParallelMergeSort(new Merger()));
+        doAnPerformanceTest(unsortedArray, new MergeSort(new Merger()));
+
+    }
+
+    private void doAnPerformanceTest(int[] unsortedArray, ISortAlgorithm sortAlgorithm){
+        //Startzeit messen
+        long start = System.nanoTime();
         //sortieren
-        ParallelMergeSort parallelMergeSort = new ParallelMergeSort(new Merger());
-        parallelMergeSort.sort(unsortedArray);
-        //Endzeit messen ParallelSort
-        long parallelEnde = System.nanoTime();
-
-        //Startzeit messen MergeSort
-        long mergeStart = System.nanoTime();
-        //sortieren
-        MergeSort mergeSort = new MergeSort(new Merger());
-        mergeSort.sort(unsortedArray);
-        //Endzeit messen MergeSort
-        long mergeEnde = System.nanoTime();
-
-        //Differenzen berechnen
-        long difMergeInMs = (mergeEnde-mergeStart)/1000000;
-        long difParalInMs = (parallelEnde-parallelStart)/1000000;
-
+        sortAlgorithm.sort(unsortedArray);
+        //Endzeit messen
+        long ende = System.nanoTime();
+        //Differenz berechnen
+        long dif= (ende-start)/1000000;
         //Differenz ausgeben
-        System.out.println("MergeSort:\t" + difMergeInMs + "ms");
-        System.out.println("ParallelSort:\t" + difParalInMs + "ms");
+        System.out.println(sortAlgorithm.getClass().getSimpleName()+":\t" + dif + "ms");
     }
 }
